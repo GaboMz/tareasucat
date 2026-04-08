eda0e738f124422c9ac1490dfce5a0e81eb4296f
GaboMZ <gabomzm@gmail.com>
2026-04-08 18:48:35 -0400

    Reorganizacion de carpetas

---
+++ Desktop/tareasucat/EstructuradeDatos/P1/main.go.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt" //Formato
+
+func main() {
+    // Slice con strings 
+    gaseosas := []string{
+        "Pepsi", 
+        "Coca Cola", 
+        "Sprite", 
+        "Fanta", 
+        "Seven Up", 
+        "Salvieti",
+    }
+
+    fmt.Println("Lista de Gaseosas:", gaseosas)
