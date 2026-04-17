0af824ad35adef613be9c3da50c7480f992e56d2
GaboMz <43045067+GaboMz@users.noreply.github.com>
2026-04-17 16:57:00 -0400

    Ejercicio de Colas en Phyton

---
+++ P5_Colas.py
@@ -0,0 +1,12 @@
+cola = []
+print("Corredores llegando a los pits")
+cola.append("Marx Verstappen")
+cola.append("Charles Leclerc")
+cola.append("Lando Norris")
+cola.append("Tsunoda")
+
+print("El orden de llegada es::", cola)
+
+pits = cola.pop(0) 
+print("Salio de los pits:", pits)
+print("Falta que salgan de los pits:", cola)
